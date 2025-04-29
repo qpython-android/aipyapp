418470e5db29cb4e3d1fd0ef0a2291a958266784
River <riverfor@gmail.com>
2025-04-29 10:17:34 +0800

    Added qpython.py

---
+++ aipyapp/aipy/qpython.py
@@ -0,0 +1,32 @@
+# -*- coding: utf8; -*-
+# qpy 环境相关函数
+import os
+import random
+import string
+
+def create_temp_py_file(code, dir):
+    # 生成一个随机的文件名
+    random_filename = ''.join(random.choices(string.ascii_letters + string.digits, k=10)) + '.py'
+    temp_file_path = os.path.join(dir, random_filename)
+    
+    # 写入代码到临时文件
+    with open(temp_file_path, 'w', encoding='utf-8') as temp_file:
+        temp_file.write(code)
+    
+    return temp_file_path
+
+def qpy_is_webapp(code):
+    # 读取前512个字节
+    header = code[:512]
+    
+    # 将字节转换为字符串并检查是否包含特定内容
+    if '#qpy:webapp' in header:
+        return True
+    else:
+        return False
+
+def qpy_exec(code, args):
+    import qpy, androidhelper
+    script = create_temp_py_file(code, qpy.tmp)
+    droid = androidhelper.Android()
+    droid.executeQPy(script)
